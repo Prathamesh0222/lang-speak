17a1ab62251425a72a48b48304ec5109cdf691b3
Prathamesh <prathameshpimpalkar2002@gmail.com>
2024-07-11 21:57:54 +0530

    text area & speech

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
-import Image from "next/image";
+"use client"
+import { TextArea } from "@/components/Inputs/TextArea";
+import "regenerator-runtime/runtime";
+import { useState } from "react";
+import { SpeechRecognitionComponent } from "@/components/SpeechRecognition/SpeechRecognition";
 
 export default function Home() {
+  const [sourceText, setSourceText] = useState<string>("");
   return (
     <div>
       <div className="h-[50rem] w-full dark:bg-black bg-white  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center">
@@ -17,7 +22,20 @@ export default function Home() {
                 <div className="mt-7 sm:mt-12 mx-auto max-w-3xl relative">
                   <div className="grid gap-4 md:grid-cols-2 grid-cols-1">
                       <div className="relative z-10 flex flex-col space-x-3 border rounded-lg shadow-lg bg-neutral-900 border-neutral-700 shadow-gray-900/20">
-                          
+                          <TextArea
+                          id="source-language"
+                          placeholder="Enter text to translate"
+                          value={sourceText}
+                          onChange={(e)=>{
+                            setSourceText(e.target.value);
+                          }}
+                          />
+                          <div className="flex flex-row justify-between w-full">
+                              <span className="cursor-pointer flex space-x-2 flex-row">
+                                <SpeechRecognitionComponent setSourceText={setSourceText}/>
+
+                              </span>
+                          </div>
                       </div>
                   </div>
                 </div>
